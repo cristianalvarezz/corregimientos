fa9751577e08694744691889fbe6c62819c486d4
cristianalvarezz <alvarescristian46@gmail.com>
2021-02-25 11:27:23 -0500

    diseño de tabla

--- application/views/accionesFormulario/mostrarCorregimientos.php
+++ application/views/accionesFormulario/mostrarCorregimientos.php
@@ -1,37 +1,38 @@
 <body>
     <!-- BARRA DE NAVEGACION -->
     <div style="height: 30px;"></div>
-    <div class="container-sm" id="datos">
+    <div class="container-fluid">
         <div class="row">
-                <div class="input-group" >           
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="bi bi-search"></i></div>
-                        </div>
-                        <input type="text" class="form-control " id="inlineFormInputGroup">
-                        <select class="form-control col-md-4 " aria-label="Default select example" name="dato">
-                            <option selected>Buscar por</option>
-                            <option value="nombrecorregimiento">Nombre del Corregimiento</option>
-                            <option value="municipio">Municipio</option>
-                            <option value="veredas">Veredas que lo componen</option>
-                            <option value="pobladores">Número de pobladores aproximado</option>
-                            <option value="ubicacionlatitud">Ubicación aproximada latitud</option>
-                            <option value="area">Area</option>
-                            <option value="longitud">Longitud</option>
-                            <option value="nautoridadprincipal">Nombre autoridad principal</option>
-                            <option value="nautoridadpolicial">Nombre autoridad de policía</option>
-                            <option value="miembrosjal">miembros JAL</option>
-                            <option value="jal">JAL </option>
-                            <option value="codigodane">Código Dane</option>
-                            <option value="numeroadministrativo">Número acto administrativo</option>
-                        </select>
-                        <button class="btn btn-light" href="<?=base_url('formulario/editar')?>" role="button">Buscar</button>
+            <div class="card shadow-lg p-3 mb-5 bg-white" id="datos">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="bi bi-search"></i></div>
+                    </div>
+                    <input type="text" class="form-control " id="inlineFormInputGroup">
+                    <select class="form-control col-md-4 " aria-label="Default select example" name="dato">
+                        <option selected>Buscar por</option>
+                        <option value="nombrecorregimiento">Nombre del Corregimiento</option>
+                        <option value="municipio">Municipio</option>
+                        <option value="veredas">Veredas que lo componen</option>
+                        <option value="pobladores">Número de pobladores aproximado</option>
+                        <option value="ubicacionlatitud">Ubicación aproximada latitud</option>
+                        <option value="area">Area</option>
+                        <option value="longitud">Longitud</option>
+                        <option value="nautoridadprincipal">Nombre autoridad principal</option>
+                        <option value="nautoridadpolicial">Nombre autoridad de policía</option>
+                        <option value="miembrosjal">miembros JAL</option>
+                        <option value="jal">JAL </option>
+                        <option value="codigodane">Código Dane</option>
+                        <option value="numeroadministrativo">Número acto administrativo</option>
+                    </select>
+                    <button class="btn btn-light" href="<?= base_url('formulario/editar') ?>" role="button">Buscar</button>
                 </div>
-              
-                <div class="table-response">
+
+                <div class="table-responsive">
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th scope="col">id</th>
+                                <th scope="col">ID</th>
                                 <th scope="col">Nombre del Corregimiento</th>
                                 <th scope="col">Municipio</th>
                                 <th scope="col">Veredas que lo componen</th>
@@ -41,7 +42,7 @@
                                 <th scope="col">Longitud</th>
                                 <th scope="col">Nombre autoridad principal</th>
                                 <th scope="col">Nombre autoridad de policía </th>
-                                <th scope="col">miembros JAL</th>
+                                <th scope="col">Miembros JAL</th>
                                 <th scope="col">JAL </th>
                                 <th scope="col">Código Dane</th>
                                 <th scope="col">Número acto administrativo</th>
@@ -53,7 +54,7 @@
                             <tr>
                                 <?php foreach ($data['corregimientos'] as $item) : ?>
                             <tr>
-                                <th scope="row"><?= $item->id_corregimiento ?></th>
+                                <td scope="row"><?= $item->id_corregimiento ?></td>
                                 <td><?= $item->nombrecorregimiento ?></td>
                                 <td><?= $item->municipio ?></td>
                                 <td><?= $item->veredas ?></td>
@@ -67,8 +68,8 @@
                                 <td><?= $item->jal ?></td>
                                 <td><?= $item->codigodane ?></td>
                                 <td><?= $item->numeroadministrativo ?></td>
-                                <td><a class="btn btn-outline-success" href="<?=base_url('formulario/editar/'.$item->id_corregimiento)?>" role="button">Editar</a> </td>    
-                                <td><a class="btn btn-outline-danger" href="#" data-id="<?=$item->id_corregimiento?>" id="delete" role="button">Eliminar</a></td>                 
+                                <td><a class="btn btn-outline-success" href="<?= base_url('formulario/editar/' . $item->id_corregimiento) ?>" role="button">Editar</a> </td>
+                                <td><a class="btn btn-outline-danger" href="#" data-id="<?= $item->id_corregimiento ?>" id="delete" role="button">Eliminar</a></td>
 
                             </tr>
                         <?php endforeach; ?>
@@ -76,26 +77,27 @@
                         </tbody>
                     </table>
                 </div>
-     
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <?php
-                    $prev = $data["current"] - 1;
-                    $next = $data["current"] + 1;
-                    if ($prev <= 0) {
-                        $prev = 1;
-                    }
-                    if ($next > $data["last_pag"]) {
-                        $next = $data["last_pag"];
-                    }
-                    ?>
-                    <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') .  $prev; ?>">Anterior</a></li>
-                    <?php for ($i = 1; $i <= $data["last_pag"]; $i++) { ?>
-                        <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') . $i; ?>"><?php echo $i; ?></a></li>
-                    <?php } ?>
-                    <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') . $next; ?>">Siguiente</a></li>
-                </ul>
-            </nav>
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <?php
+                        $prev = $data["current"] - 1;
+                        $next = $data["current"] + 1;
+                        if ($prev <= 0) {
+                            $prev = 1;
+                        }
+                        if ($next > $data["last_pag"]) {
+                            $next = $data["last_pag"];
+                        }
+                        ?>
+                        <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') .  $prev; ?>">Anterior</a></li>
+                        <?php for ($i = 1; $i <= $data["last_pag"]; $i++) { ?>
+                            <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') . $i; ?>"><?php echo $i; ?></a></li>
+                        <?php } ?>
+                        <li class="page-item"><a class="page-link" href="<?php echo base_url('formulario/mostrarCorregimientos/') . $next; ?>">Siguiente</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </div>
 </body>
--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es-mx">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-  
-    <link rel="stylesheet" href="<?=base_url('assets/libs/sweetalert2/dist/sweetalert2.min.css')?>">
-    <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('assets/libs/sweetalert2/dist/sweetalert2.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
     <title>Formulario de corregimientos</title>
 </head>
